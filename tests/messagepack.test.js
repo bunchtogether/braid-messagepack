d4e024adf616894aec13193fce2ab408843360f4
John Wehr <johnwehr@gmail.com>
2019-03-17 23:43:28 -0400

    Initial commit

---
+++ tests/messagepack.test.js
@@ -0,0 +1,174 @@
+// @flow
+
+const crypto = require('crypto');
+const uuid = require('uuid');
+const { ObservedRemoveMap, ObservedRemoveSet } = require('observed-remove');
+const {
+  Credentials,
+  CredentialsResponse,
+  PeerRequest,
+  PeerResponse,
+  Unpeer,
+  SubscribeRequest,
+  SubscribeResponse,
+  Unsubscribe,
+  DataDump,
+  ProviderDump,
+  ActiveProviderDump,
+  PeerDump,
+  PeerSubscriptionDump,
+  encode,
+  decode,
+} = require('../src');
+
+function randomInteger() {
+  return crypto.randomBytes(4).readUInt32BE(0, true);
+}
+
+describe('Messagepack', () => {
+  test('Should encode and decode credentials', async () => {
+    const value = {
+      [uuid.v4()]: uuid.v4(),
+    };
+    const credentials = new Credentials(value);
+    const encoded = encode(credentials);
+    const decoded = decode(encoded);
+    expect(decoded).toBeInstanceOf(Credentials);
+    expect(value).toEqual(decoded.value);
+  });
+  test('Should encode and decode credentials responses', async () => {
+    const success = Math.random() > 0.5;
+    const code = Math.floor(Math.random() * 1000);
+    const message = uuid.v4();
+    const credentialsResponse = new CredentialsResponse({ success, code, message });
+    const encoded = encode(credentialsResponse);
+    const decoded = decode(encoded);
+    expect(decoded).toBeInstanceOf(CredentialsResponse);
+    expect({ success, code, message }).toEqual(decoded.value);
+  });
+  test('Should encode and decode peer requests', async () => {
+    const peerId = uuid.v4();
+    const peerRequest = new PeerRequest(peerId);
+    const encoded = encode(peerRequest);
+    const decoded = decode(encoded);
+    expect(decoded).toBeInstanceOf(PeerRequest);
+    expect(peerId).toEqual(decoded.value);
+  });
+  test('Should encode and decode peer responses', async () => {
+    const id = uuid.v4();
+    const success = Math.random() > 0.5;
+    const code = Math.floor(Math.random() * 1000);
+    const message = uuid.v4();
+    const peerResponse = new PeerResponse({ id, success, code, message });
+    const encoded = encode(peerResponse);
+    const decoded = decode(encoded);
+    expect(decoded).toBeInstanceOf(PeerResponse);
+    expect({ id, success, code, message }).toEqual(decoded.value);
+  });
+  test('Should encode and decode unpeers', async () => {
+    const unpeer = new Unpeer();
+    const encoded = encode(unpeer);
+    const decoded = decode(encoded);
+    expect(decoded).toBeInstanceOf(Unpeer);
+  });
+  test('Should encode and decode subscribe requests', async () => {
+    const key = uuid.v4();
+    const subscribeRequest = new SubscribeRequest(key);
+    const encoded = encode(subscribeRequest);
+    const decoded = decode(encoded);
+    expect(decoded).toBeInstanceOf(SubscribeRequest);
+    expect(key).toEqual(decoded.value);
+  });
+  test('Should encode and decode subscribe responses', async () => {
+    const key = uuid.v4();
+    const success = Math.random() > 0.5;
+    const code = Math.floor(Math.random() * 1000);
+    const message = uuid.v4();
+    const subscribeResponse = new SubscribeResponse({ key, success, code, message });
+    const encoded = encode(subscribeResponse);
+    const decoded = decode(encoded);
+    expect(decoded).toBeInstanceOf(SubscribeResponse);
+    expect({ key, success, code, message }).toEqual(decoded.value);
+  });
+  test('Should encode and decode unsubscribes', async () => {
+    const key = uuid.v4();
+    const unsubscribe = new Unsubscribe(key);
+    const encoded = encode(unsubscribe);
+    const decoded = decode(encoded);
+    expect(decoded).toBeInstanceOf(Unsubscribe);
+    expect(key).toEqual(decoded.value);
+  });
+  test('Should encode and decode data dumps', async () => {
+    const alice = new ObservedRemoveMap();
+    const bob = new ObservedRemoveMap();
+    const key = uuid.v4();
+    const value = {
+      [uuid.v4()]: uuid.v4(),
+    };
+    alice.set(key, value);
+    const ids = [randomInteger()];
+    const dataDump = new DataDump(alice.dump(), ids);
+    const encoded = encode(dataDump);
+    const decoded = decode(encoded);
+    expect(decoded).toBeInstanceOf(DataDump);
+    bob.process(decoded.queue);
+    expect(bob.get(key)).toEqual(value);
+    expect(ids).toEqual(decoded.ids);
+  });
+  test('Should encode and decode provider dumps', async () => {
+    const alice = new ObservedRemoveSet();
+    const bob = new ObservedRemoveSet();
+    const key = uuid.v4();
+    alice.add(key);
+    const ids = [randomInteger()];
+    const providerDump = new ProviderDump(alice.dump(), ids);
+    const encoded = encode(providerDump);
+    const decoded = decode(encoded);
+    expect(decoded).toBeInstanceOf(ProviderDump);
+    bob.process(decoded.queue);
+    expect(bob.has(key)).toEqual(true);
+    expect(ids).toEqual(decoded.ids);
+  });
+  test('Should encode and decode listener dumps', async () => {
+    const alice = new ObservedRemoveSet();
+    const bob = new ObservedRemoveSet();
+    const key = uuid.v4();
+    alice.add(key);
+    const ids = [randomInteger()];
+    const listenerDump = new ActiveProviderDump(alice.dump(), ids);
+    const encoded = encode(listenerDump);
+    const decoded = decode(encoded);
+    expect(decoded).toBeInstanceOf(ActiveProviderDump);
+    bob.process(decoded.queue);
+    expect(bob.has(key)).toEqual(true);
+    expect(ids).toEqual(decoded.ids);
+  });
+  test('Should encode and decode peer dumps', async () => {
+    const alice = new ObservedRemoveSet();
+    const bob = new ObservedRemoveSet();
+    const key = uuid.v4();
+    alice.add(key);
+    const ids = [randomInteger()];
+    const peerDump = new PeerDump(alice.dump(), ids);
+    const encoded = encode(peerDump);
+    const decoded = decode(encoded);
+    expect(decoded).toBeInstanceOf(PeerDump);
+    bob.process(decoded.queue);
+    expect(bob.has(key)).toEqual(true);
+    expect(ids).toEqual(decoded.ids);
+  });
+  test('Should encode and decode subscriber dumps', async () => {
+    const alice = new ObservedRemoveSet();
+    const bob = new ObservedRemoveSet();
+    const key = uuid.v4();
+    alice.add(key);
+    const ids = [randomInteger()];
+    const peerDump = new PeerSubscriptionDump(alice.dump(), ids);
+    const encoded = encode(peerDump);
+    const decoded = decode(encoded);
+    expect(decoded).toBeInstanceOf(PeerSubscriptionDump);
+    bob.process(decoded.queue);
+    expect(bob.has(key)).toEqual(true);
+    expect(ids).toEqual(decoded.ids);
+  });
+});
